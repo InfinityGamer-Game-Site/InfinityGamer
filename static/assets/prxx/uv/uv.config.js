f24e0401586ad4b4673d81bd9437fafc044b1e94
InfinityLoop1 <enzopassini@icloud.com>
2024-10-20 18:16:39 -0500

    im stupid

--- static/assets/prxx/uv/uv.config.js
+++ static/assets/prxx/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: 'output',
+    prefix: 'output/',
     bare:'https://skibzy.global.ssl.fastly.net/bear/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
